e5aee5474026db078ad15765eb7f1cc31b502783
karelmonash2021 <91338675+karelmonash2021@users.noreply.github.com>
2021-10-10 11:54:32 +0700

    chloro added

---
+++ chloropath.js
@@ -0,0 +1,10 @@
+var vg_1 = "flow_map.vg.json"
+var vg_2 = "departVarrival.vg.json"
+
+vegaEmbed("#chloropleth_map",vg_1).then(function(result){
+
+}).catch(console.error);
+
+vegaEmbed("#bubble_chart",vg_2).then(function(result){
+
+}).catch(console.error);
