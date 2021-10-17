71f4fecb3c3e6974d16e71893e95ba5ddcc25193
karelmonash2021 <91338675+karelmonash2021@users.noreply.github.com>
2021-10-18 05:10:27 +0700

    Update chloropath.js

--- chloropath.js
+++ chloropath.js
@@ -1,10 +1,25 @@
-var vg_1 = "chloroplat_map.vg.json"
+var vg_1 = "flow_map.vg.json"
 var vg_2 = "departVarrival.vg.json"
+var vg_3 = "line_chart.vg.json"
+var vg_4 = "area_chart.vg.json"
+var vg_5 = "airline_fleet_comparison.vg.json"
 
-vegaEmbed("#chloropleth_map",vg_1).then(function(result){
+vegaEmbed("#flow_map",vg_1).then(function(result){
 
 }).catch(console.error);
 
 vegaEmbed("#bubble_chart",vg_2).then(function(result){
+
+}).catch(console.error);
+
+vegaEmbed("#line_chart",vg_3).then(function(result){
+
+}).catch(console.error);
+
+vegaEmbed("#area_chart",vg_4).then(function(result){
+
+}).catch(console.error);
+
+vegaEmbed("#bar_chart",vg_5).then(function(result){
 
 }).catch(console.error);
